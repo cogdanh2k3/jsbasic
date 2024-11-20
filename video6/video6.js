307a0181bfe14c801ef0d29bd0600eb228ae1d07
Danh <cogdanh2k3@gmail.com>
2024-11-20 21:36:05 +0700

    source code, lession10

---
+++ video6/video6.js
@@ -0,0 +1,15 @@
+console.log('Hello World from HTML'.toUpperCase());
+console.log('Hello World from HTML'.toLowerCase());
+let a = 5; 
+let b = 6;
+let c = 6.9;
+let d = '5';
+let e = Number(d); // +d
+console.log(a + b);
+console.log(a + c);
+console.log(a + d);
+console.log(a * d);
+console.log(a - d);
+console.log('e value: ', e, 'type: ', typeof e);
+console.log(a === e,'a: ', typeof a,"e: ", typeof e);
+
