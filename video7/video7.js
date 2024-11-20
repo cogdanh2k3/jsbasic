a09a38d668a4876080bc0ec8c8fc9a853443d664
Danh <cogdanh2k3@gmail.com>
2024-11-20 22:09:59 +0700

    source code, lession11

---
+++ video7/video7.js
@@ -0,0 +1,9 @@
+console.log('Hello World from HTML');
+let a = 5;
+let b = 10;
+
+console.log('Fifteen is ' + (a+b) + ' and\nnot ' + (2*a+b) + '.');
+console.log('Fifteen is ' + a+b + ' and not ' + (2*a+b) + '.');
+
+console.log(`Fifteen is ${a+b} and
+not ${2*a+b}`);
