288ad0915cfd70cf1a287d147478c3da3bdd90d6
Danh <cogdanh2k3@gmail.com>
2024-11-20 18:37:11 +0700

    source code, lession7

---
+++ video3/video3.js
@@ -0,0 +1,8 @@
+
+const a = 7;
+
+console.log(`Hello World: `, a)
+
+let x = 5, y = 6, z = 7;
+console.log("Test String", 'x = ', x, 'y = ', y, 'z = ', z)
+console.log("Test String" +'x = ' +  x +  'y = ' + y + 'z = ' +  z)
