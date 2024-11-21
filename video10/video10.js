27ad109748a327b534d63441cae2a1770e6ed726
Danh <cogdanh2k3@gmail.com>
2024-11-21 20:57:47 +0700

    source code, lession14

---
+++ video10/video10.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = '5';
+
+console.log('Compare a == b: ', a == b); 
+console.log('Compare a === b: ', a === b);
+
+// Empty, null, underfined
+let c = ""; 
+console.log('check value: ', c);
+
+let d = '';
+d = 5;
+d = [ '11']
+
+//
---
+++ video11/video11.js
@@ -0,0 +1,26 @@
+console.log('Hello World');
+
+for(let i = 0; i < 2; i++){
+    console.log('check var i = ', i);
+    
+}
+
+let arr = ['MU','Liver', 'Chelsea', 'ManCity']
+for(let i = 0; i < arr.length; i++){
+    console.log('check var i = ', arr[i]);
+    
+}
+
+//=============================
+var i = 0
+for(var i = 0; i < arr.length; i++){
+    console.log('check var i = ', i);
+}
+console.log(i);
+
+//============================
+let j = 0;
+for(let j = 0; j < arr.length; j++){
+    console.log('check var j = ', j);
+}
+console.log(j);
