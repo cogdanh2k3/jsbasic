66f13de832ea5922903d14ef568cb22d6d179117
Danh <cogdanh2k3@gmail.com>
2024-11-21 14:42:49 +0700

    source code, lession12

---
+++ video8/video8.js
@@ -0,0 +1,23 @@
+console.log('Hello World');
+
+//key: value 
+let obj = {
+    name: 'Danh', address: 'Viet Nam',
+    // a: function(){
+    //     console.log('Hello World inside function');
+    //     return ''
+    // }
+};
+
+//JSON
+// "{name : 'Eric', address: 'Ha Noi'}"
+
+let b = "Danh"
+
+obj.b = 'Ali' // Add b: Ali
+console.log('type of obj: ',typeof obj, 'type of b: ',typeof b);
+console.log(`What is ur name? `, `My name's: `, obj.name);
+console.log(`Where are u from? `, `I'm from: `, obj.address);
+console.log(`What is ur name? `, `My name's: `, obj['name']);
+console.log(`Where are u from? `, `I'm from: `, obj['address']);
+
